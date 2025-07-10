e0392885a76a775ef37dbb8413dc961dc0e440bf
Asadbek <hoshimovaustin0550@gmail.com>
2025-07-10 19:23:50 +0900

    add Question and Answer models, configure admin settings

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
+from .models import Question, Answer
 # Register your models here.
+
+
+class AnswerInlineModel(admin.TabularInline):
+    model = Answer
+    fields = [
+        "answer",
+        "is_correct",
+    ]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = [
+        "title",
+        "points",
+        "difficulty",
+    ]
+    list_display = [
+        "title",
+        "updated_at",
+    ]
+    inlines = [
+        AnswerInlineModel,
+    ]
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "answer",
+        "is_correct",
+        "question",
+]
