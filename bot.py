a97c009f46922bb5a4343c977da1e458de10f6b0
Asadbek <hoshimovaustin0550@gmail.com>
2025-07-11 17:32:10 +0900

    Show question on user question

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import discord
 from environs import Env # new
 from dotenv import load_dotenv
+import requests
+import os
+import json
+import asyncio
 
 load_dotenv()
 
@@ -14,6 +18,24 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+def get_question():
+    qs = ''
+    id = 1
+    answer = 0
+    response = requests.get("http://127.0.0.1:8000/api/random/")
+    json_data = json.loads(response.text)
+    qs += "Question: \n"
+    qs += json_data[0]['title'] + '\n'
+
+    for item in json_data[0]['answer']:
+        qs += str(id) + '. ' + item['answer'] + "\n"
+
+        if item['is_correct']:
+            answer = id
+        id += 1
+
+    return(qs, answer)
+
 
 @client.event
 async def on_ready():
@@ -24,8 +46,21 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith("hello"):
-        await message.channel.send("hello, I am a bot")
+    if message.content.startswith("$question"):
+        qs, answer = get_question()
+        await message.channel.send(qs)
 
+        def check(m):
+            return m.author == message.author and m.content.isdigit()
+
+        try:
+            guess = await client.wait_for('message', check=check, timeout=2.0)
+        except asyncio.TimeoutError:
+            return await message.channel.send("Sorry, you took too long")
+
+        if int(guess.content) == answer:
+            await message.channel.send("You are right!")
+        else:
+            await message.channel.send("Oops. That is not right.")
 
 client.run(os.getenv("DISCORD_TOKEN"))
