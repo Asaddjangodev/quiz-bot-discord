e0674fc2736e1d6d7d0233ad8dd72a2a926f9c30
Asadbek <hoshimovaustin0550@gmail.com>
2025-07-11 17:47:47 +0900

    Set up web server

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ def get_question():
     qs = ''
     id = 1
     answer = 0
-    response = requests.get("http://127.0.0.1:8000/api/random/")
+    response = requests.get("https://aqueous-thicket-58289-3c131321ece5.herokuapp.com/api/random/")
     json_data = json.loads(response.text)
     qs += "Question: \n"
     qs += json_data[0]['title'] + '\n'
--- quizbot/settings.py
+++ quizbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-6$4y)f^go&yj(oedynz_+y%&7$7v9zbx!+@+x%ly!8br*s)gxv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
